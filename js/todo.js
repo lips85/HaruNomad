43a775b6adc6cad6a11aaf42c66b03d223bca608
JWYOO <godjaewook@naver.com>
2024-01-07 09:34:03 +0900

    todo list

---
+++ js/todo.js
@@ -0,0 +1,4 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+const toDoInput = document.querySelector("#todo-form input");
+// const toDos = [];
